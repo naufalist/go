870872f5cb3501b46369eb94c631ca14b23933cf
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2021-01-07 15:35:39 +0700

    test reader

--- ~exp/main.go
+++ ~exp/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func init() {
+	fmt.Println("--- test reader ---")
+}
 
 func main() {
-	fmt.Println("tes")
-	fmt.Println("tes1")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Input text: ")
+	text, _ := reader.ReadString('\n')
+	// fmt.Println("This is your text:", text)
+	fmt.Printf("This is your text: %s", text)
+
 }
