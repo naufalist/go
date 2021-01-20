4e84a510d85ce6025d58e3c137a6880f1458f299
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2021-01-20 20:32:21 +0700

    pointer cont.

--- pointer/main.go
+++ pointer/main.go
@@ -3,15 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	numberA := 5
-	numberB := &numberA
+	// numberA := 5
+	// numberB := &numberA
+
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
+
+	// *numberB = 10
+
+	// fmt.Println(*numberB)
+	// fmt.Println(numberA)
+
+	var numberA int = 5
+	var numberB *int = &numberA
 
 	fmt.Println(numberA)
 	fmt.Println(numberB)
 	fmt.Println(*numberB)
 
-	*numberB = 10
+	numberA = 20
 
-	fmt.Println(*numberB)
 	fmt.Println(numberA)
+	fmt.Println(numberB)
+	fmt.Println(*numberB)
 }
