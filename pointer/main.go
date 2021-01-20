ea0c8ca0cea65e583d800ed89eee92a5616d1264
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2021-01-20 20:44:42 +0700

    pointer cont.

--- pointer/main.go
+++ pointer/main.go
@@ -15,16 +15,29 @@ func main() {
 	// fmt.Println(*numberB)
 	// fmt.Println(numberA)
 
-	var numberA int = 5
-	var numberB *int = &numberA
+	// var numberA int = 5
+	// var numberB *int = &numberA
 
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB)
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
 
-	numberA = 20
+	// numberA = 20
 
-	fmt.Println(numberA)
-	fmt.Println(numberB)
-	fmt.Println(*numberB)
+	// fmt.Println(numberA)
+	// fmt.Println(numberB)
+	// fmt.Println(*numberB)
+
+	number := 5
+	fmt.Println("Alamat memory :", &number)
+	fmt.Println("Nilai awal :", number)
+	change(&number, 100)
+	fmt.Println("Nilai akhir :", number)
+	fmt.Println("Alamat memory :", &number)
+}
+
+func change(old *int, new int) {
+	*old = new
+	fmt.Println("Alamat memory :", old)
+	fmt.Println("Di dalam function :", old)
 }
